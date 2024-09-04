71b80a73d6406ffb9677d30bbeaf0899b7ac5180
Subash <126191558+Subash-456@users.noreply.github.com>
2024-09-04 13:55:15 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,15 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
